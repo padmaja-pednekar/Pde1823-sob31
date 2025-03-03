c2e6ba9179008bb55d8528fb67e03d979c965438
padmaja-pednekar <pp937@live.mdx.ac.uk>
2025-03-03 12:04:24 +0400

    Changes made in the cowbulls_incomplete.py file successfully.

--- cowbulls_incomplete.py
+++ cowbulls_incomplete.py
@@ -1,11 +1,18 @@
 import random
 
 def compare_numbers(number, user_guess):
-    ## your code here
-    return cowbull
+    cows = 0 #padma_pednekar added extra lines of code
+    bulls = 0
+
+    for i in range (len(number)):
+        if user_guess[i] == number[i]:
+            bulls += 1
+        elif user_guess[i] == number[i]:
+            cows += 1
+    return cows,bulls #padma_pednekar made the returns the right statement, by changing "cowbull" to "cows, bulls"
 
 playing = True #gotta play the game
-number = int(random.randint(0,9999)) #random 4 digit number #padma_pednekar changed str() to int()
+number = str(random.randint(0,9999)) #random 4 digit number
 guesses = 0
 print (number) #padma_pednekar added paranthesis () to the print statement
 
@@ -16,7 +23,7 @@ print("The game ends when you get 4 bulls!")
 print("Type exit at any prompt to exit.")
 
 while playing:
-    user_guess = raw_input("Give me your best guess!")
+    user_guess = input("Give me your best guess!") #padma_pednekar changed raw_input to input
     if user_guess == "exit":
         break
     cowbullcount = compare_numbers(number,user_guess)
@@ -26,7 +33,7 @@ while playing:
 
     if cowbullcount[1]==4:
         playing = False
-        print("You win the game after " + str(guesses) + "! The number was "+str(number))
+        print("You win the game after " + str(guesses) + "guesses! The number was "+str(number)) #padma_pednekar modified the print statement and added 'guesses'
         break #redundant exit
     else:
         print("Your guess isn't quite right, try again.")
