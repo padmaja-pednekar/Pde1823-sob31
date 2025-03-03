2ff5d4544ebd14a733ed13570ce18adfb62e03a2
padmaja-pednekar <pp937@live.mdx.ac.uk>
2025-03-03 11:12:33 +0400

    Changes made in the broken_pirate.py file successfully.

--- broken_pirate.py
+++ broken_pirate.py
@@ -1,5 +1,5 @@
-greeting = input("Hello, possible pirate! What's the password?)
-if greeting in ["Arrr!"):
+greeting = input("Hello, possible pirate! What's the password?") #padma-pednekar added double quotes (") symbol
+if greeting in ["Arrr!"]: #padma-pednekar changed paranthesis ) to square bracket ]
 	print("Go away, pirate.")
-elif
-print("Greetings, hater of pirates!")
+else: #padma-pednekar changed elif to else; added colon (:) symbol
+	print("Greetings, hater of pirates!") #padma-pednekar added indentation
